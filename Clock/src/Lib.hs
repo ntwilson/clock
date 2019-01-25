0d2d0d40b7e46fe20a23108c1545a5bcf873dd8a
nathan <ntwilson@gmail.com>
2019-01-25 00:49:21 -0600

    initial project

---
+++ Clock/src/Lib.hs
@@ -0,0 +1,118 @@
+module Lib (someFunc) where
+
+import Paths_Clock (getDataFileName)
+
+import System.Environment (getArgs)
+import System.Process (runCommand, waitForProcess)
+import GHC.IO.Exception (ExitCode(..))
+import Data.List.Split (splitOn)
+import qualified Data.List as List
+import qualified Safe as List
+import qualified Data.Time.LocalTime as Time
+import qualified Data.Time.Clock as Time
+import qualified Data.Time.Format as Time
+import Data.Time.Format.ISO8601 (iso8601Show, iso8601ParseM)
+
+timesheetRelativePath :: String
+timesheetRelativePath = "data\\timesheet.csv"
+
+timesheetPath :: IO FilePath
+timesheetPath = getDataFileName timesheetRelativePath
+
+timesheetContents :: IO [[String]]
+timesheetContents = do
+  filePath <- timesheetPath
+  fileLines <- lines <$> readFile filePath 
+  pure $ splitOn "," <$> fileLines
+
+appendRow :: [String] -> IO ()
+appendRow row = do
+  let line = List.concat row
+  filePath <- timesheetPath
+  appendFile filePath $ line
+
+data ActionType = ClockIn | ClockOut | Launch | Unrecognized
+
+action :: [String] -> ActionType
+action ("in":_) = ClockIn
+action ("out":_) = ClockOut
+action ("open":_) = Launch
+action _ = Unrecognized
+
+-- let inTime filename =
+  --   let inStr = 
+    --     readFile filename
+    --     |> Seq.last |> Seq.head
+    
+    --   Convert.ToDateTime inStr
+
+timeOfClockIn :: IO (Maybe Time.ZonedTime)
+timeOfClockIn = do
+  contents <- timesheetContents
+  let date = strToDate =<< List.headMay =<< List.lastMay contents
+  pure date
+
+  where
+    strToDate dateStr =
+      iso8601ParseM dateStr
+
+  
+clockedOut :: [[String]] -> Bool
+clockedOut fileLines = 
+  case List.lastMay fileLines of
+    Just last -> List.length last > 1
+    Nothing -> True
+    
+clockedIn :: [[String]] -> Bool
+clockedIn = not . clockedOut
+
+clockIn :: IO (Either String String)
+clockIn = do
+  contents <- timesheetContents
+  if clockedIn contents 
+  then pure $ Left "didn't do anything: you're already clocked in!"
+  else do
+    now <- Time.getZonedTime
+    appendRow ["\n" ++ iso8601Show now]
+    pure $ Right $ "Clocked in at: " ++ iso8601Show now
+
+clockOut :: IO (Either String String)
+clockOut = do
+  contents <- timesheetContents
+  if clockedOut contents
+  then pure $ Left "didn't do anything: you aren't clocked in!"
+  else do
+    now <- Time.getZonedTime
+    maybeInTime <- timeOfClockIn
+    case maybeInTime of 
+      Just inTime -> do
+        let 
+          diff = Time.diffUTCTime (Time.zonedTimeToUTC now) (Time.zonedTimeToUTC inTime)
+          diffStr = Time.formatTime Time.defaultTimeLocale "%h:%M%ES" diff
+
+        appendRow [",", iso8601Show now, ",", diffStr]
+        pure $ Right $ "Clocked out; elapsed time: " ++ diffStr
+
+openTimesheet :: IO (Either String String)
+openTimesheet = do
+  exitCode <- waitForProcess =<< runCommand =<< timesheetPath
+  case exitCode of
+    ExitSuccess -> pure $ Right "done"
+    ExitFailure code -> pure $ Left $ "Exit code: " ++ show code
+
+
+someFunc :: IO ()
+someFunc = do
+  args <- getArgs
+  result <- 
+    case action args of
+      ClockIn -> clockIn
+      ClockOut -> clockOut
+      Launch -> openTimesheet
+      Unrecognized -> pure $ Left "huh?"
+
+  case result of 
+    Right str -> putStrLn str
+    Left str -> putStrLn $ "ERR: " ++ str
+
+  
